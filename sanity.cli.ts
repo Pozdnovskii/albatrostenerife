0d5dfeccce9d834159c8051d38bec62820a09bee
Ivan Pozdnovskii <ipozdnovskii@icloud.com>
2026-04-29 14:59:49 +0300

    Sanity upgrade

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,20 @@
 import { defineCliConfig } from "sanity/cli";
+import path from "path";
 
 export default defineCliConfig({
   api: {
     projectId: "nr9v3mei",
     dataset: "production",
+  },
+  deployment: {
+    appId: "o7xvpfo5pgvda4grkkdo1dzo",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@studio": path.resolve("./studio"),
+        "@i18n": path.resolve("./src/i18n"),
+      },
+    },
   },
 });
